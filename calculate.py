2be9d5d3fdeaf848614f09a39b5390b07caa3bff
Monika Birgiel Marszal <monikabirgiel@gmail.com>
2023-03-04 08:22:17 +0100

    Logs added

--- calculate.py
+++ calculate.py
@@ -17,22 +17,25 @@ def calculation(type, number1, number2):
         if type_no == "4":
             if number2 != 0:
                 result = round(number1 / number2, 2)
+                logging.info("Dzielę", number1, " przez ", number2, " Wynik to: ", result)
             else:
                 logging.info("Mama mawiała: Nie dziel przez zero cholero!")
                 exit(1)
-            logging.info("Dzielę", number1, " przez ", number2, " Wynik to: ", result)
-        else:
+        else:    
             exit(1)
 
-#calculation("1",1,1)
+#calculation("1",1,1) - spraqdzenie funkcji
 
 
 if __name__ == "__main__":
     type = input("Podaj rodzaj operacji: 1-dodawanie, 2-odejmowanie, 3-mnozenie, 4-dzielenie: ")
+    logging.info("The programme is calling with first parameter %s" %type)
     number1 = input("Podaj pierwszą liczbę działania: ")
     number1 = float(number1)
+    logging.info("First variable given: %s" %number1)
     number2 = input("Podaj drugą liczbę działania: ")
     number2 = float(number2)
+    logging.info("Second variable given: %s" %number1)
     type_no = ["1", "2", "3", "4"]
     for type_no in type:
         if type_no == "1":
@@ -50,14 +53,12 @@ if __name__ == "__main__":
                 print("Dzielęc", number1, " przez ", number2, "Wynik to: ", result)
             else:
                 print("Mama mawiała: Nie dziel przez zero cholero!")
+                logging.info("Second variable given is zero")
+                exit(1)
         else:
             exit(1)
 
-    #logging.debug("The program was called with this parameter - action type %s" % sys.argv[1])
-    #logging.debug("The program was called with these parameters - numbers %s" % sys.argv[1:])
-    #type_no = str(sys.argv[1])
-    #number1 = int(sys.argv[1])
-    #number2 = int(sys.argv[2])
+
 
  
 
